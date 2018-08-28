9815f7ac6f5181651d9f9eb2deb04adc2d271979
pope-of-nope@github.com <louis.gragnani@gmail.com>
2018-08-28 13:00:01 -0400

    wip

--- core.py
+++ core.py
@@ -3,7 +3,7 @@ import os
 import shutil
 from configparser import ConfigParser
 import logging
-from typing import Dict, List
+from typing import Dict, List, Generator
 
 logger = logging.Logger("psql_utils")
 logger.setLevel(logging.DEBUG)
@@ -44,39 +44,110 @@ class Config(object):
         self.config = self.__get()
 
 
-class PostgresUser(object):
-    def __init__(self, username, password):
-        # type: (str, str)->None
+# class PostgresUser(object):
+#     def __init__(self, username, password):
+#         # type: (str, str)->None
+#         self.username = username
+#         self.password = password
+#
+#     def serialize(self):
+#         return {"username": self.username, "password": self.password}
+#
+#     @classmethod
+#     def deserialize(cls, data):
+#         if isinstance(data, cls):
+#             return data
+#         elif isinstance(data, dict):
+#             return cls(**data)
+#         elif isinstance(data, str):
+#             return cls(**json.loads(data))
+#         elif isinstance(data, list):
+#             return [cls.deserialize(d) for d in data]
+#
+#
+# class Server(object):
+#     def __init__(self, name, version, host='localhost', port=5432, users=list()):
+#         # type: (str, str, str, int, List[PostgresUser])->None
+#         self.name = name
+#         self.version = version
+#         self.host = host
+#         self.port = port
+#         self.users = [PostgresUser.deserialize(u) for u in users]
+#
+#     def serialize(self):
+#         return {"name": self.name, "version": self.version, "host": self.host, "port": self.port,
+#                 "users": [u.serialize() for u in self.users]}
+#
+#     @classmethod
+#     def deserialize(cls, data):
+#         if isinstance(data, cls):
+#             return data
+#         elif isinstance(data, dict):
+#             return cls(**data)
+#         elif isinstance(data, str):
+#             return cls(**json.loads(data))
+#         elif isinstance(data, list):
+#             return [cls.deserialize(d) for d in data]
+
+
+class PGPassEntry(object):
+    def __init__(self, hostname, port, db, username, password):
+        # type: (str, int, str, str, str)->None
+        self.hostname = hostname
+        self.port = port
+        self.db = db
         self.username = username
         self.password = password
 
-    def serialize(self):
-        return {"username": self.username, "password": self.password}
-
     @classmethod
-    def deserialize(cls, data):
-        if isinstance(data, cls):
-            return data
-        elif isinstance(data, dict):
-            return cls(**data)
-        elif isinstance(data, str):
-            return cls(**json.loads(data))
-        elif isinstance(data, list):
-            return [cls.deserialize(d) for d in data]
+    def from_line(cls, entry_line):
+        # type: (str)->PGPassEntry
+        args = entry_line.strip().split(":")
+        hostname = args[0]
+        port = int(args[1])
+        db = args[2]
+        username = args[3]
+        password = args[4]
+        return cls(hostname, port, db, username, password)
+
+    def to_line(self):
+        return ":".join([self.hostname, str(self.port), self.db, self.username, self.password])
+
+
+class PGPassFile(object):
+    _file = ".pgpass"
+
+    def __init__(self, entries=list()):
+        # type: (List[PGPassEntry])->None
+        self.__entries = entries
+        self.load()
+
+    def load(self):
+        if not os.path.isfile(self._file):
+            self.save()
+        with open(self._file, 'r', encoding='utf8') as f:
+            self.__entries = [PGPassEntry.from_line(line) for line in f]
+
+    def save(self):
+        with open(self._file, 'w', encoding='utf8') as f:
+            f.write("\n".join([e.to_line() for e in self.__entries]))
+
+    def __iter__(self):
+        # type: ()->Generator[PGPassEntry]
+        for item in self.__entries:
+            yield item
 
 
 class Server(object):
-    def __init__(self, name, version, host='localhost', port=5432, users=list()):
-        # type: (str, str, str, int, List[PostgresUser])->None
+    def __init__(self, name, version, host='localhost', port=5432):
+        # type: (str, str, str, int)->None
         self.name = name
         self.version = version
         self.host = host
         self.port = port
-        self.users = [PostgresUser.deserialize(u) for u in users]
 
     def serialize(self):
-        return {"name": self.name, "version": self.version, "host": self.host, "port": self.port,
-                "users": [u.serialize() for u in self.users]}
+        return {"name": self.name, "version": self.version, "host": self.host, "port": self.port}
 
     @classmethod
     def deserialize(cls, data):
