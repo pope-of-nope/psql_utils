0b905fe2e3d164fdfd24501bd25b045382717cb6
pope-of-nope@github.com <louis.gragnani@gmail.com>
2018-08-28 11:40:53 -0400

    readme disclaimer.

--- core.py
+++ core.py
@@ -1,20 +1,60 @@
+import os
+import shutil
 from configparser import ConfigParser
+import logging
+
+logger = logging.Logger("psql_utils")
+logger.setLevel(logging.DEBUG)
 
 
 class Config(object):
+    template_config = "config-template.ini"
     config_file = "config.ini"
 
     @classmethod
-    def get(cls):
+    def __ensure_config_file_exists(cls):
+        if not os.path.isfile(cls.template_config):
+            error = "Unable to locate template config file: %s" % cls.template_config
+            logger.error(error)
+            raise FileNotFoundError(error)
+        if not os.path.isfile(cls.config_file):
+            logger.info("Copying new config file '%s' from template config file '%s'" % (
+                cls.config_file, cls.template_config
+            ))
+            try:
+                shutil.copy(cls.template_config, cls.config_file)
+            except Exception as e:
+                logger.error(e)
+                raise e
+            else:
+                logger.info("...successfully created config file.")
+
+    @classmethod
+    def __get(cls):
         # type: ()->ConfigParser
+        cls.__ensure_config_file_exists()
         config = ConfigParser()
         with open(cls.config_file, 'r', encoding='utf-8') as f:
             return config.read_file(f)
 
+    def __init__(self):
+        # type: ()->None
+        self.config = self.__get()
 
-class Environment(object):
-    def __init__(self, config):
-        # type: (ConfigParser)->None
-        self.config = config
 
-    def check(self):
+class Environments(object):
+    _environments_file = "environments.json"
+
+    def __init__(self):
+        # type: (str)->None
+
+
+        def find_postgres_versions():
+
+
+
+class Interface(object):
+    config = Config()
+
+    def __init__(self):
+        self.env = Environment(self.config)
